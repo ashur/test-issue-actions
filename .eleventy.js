d0064ac3cc233c09fbc14191609453bf7978e4d4
Ashur Cabrera <hello@ashur.cab>
2022-03-22 09:16:19 -0600

    Update feed metadata

--- .eleventy.js
+++ .eleventy.js
@@ -19,42 +19,28 @@ module.exports = (eleventyConfig) =>
 	eleventyConfig.addDataExtension("yml", contents => yaml.load(contents));
 
 	/* Filters */
-	eleventyConfig.addFilter("feed", (posts, format) => {
+	eleventyConfig.addFilter("feed", (posts, metadata, format) => {
 		const feed = new Feed({
-		  title: "Feed Title",
-		  description: "This is my personal feed!",
-		  id: "http://example.com/",
-		  link: "http://example.com/",
-		  language: "en", // optional, used only in RSS 2.0, possible values: http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
-		  image: "http://example.com/image.png",
-		  favicon: "http://example.com/favicon.ico",
-		  copyright: "All rights reserved 2013, John Doe",
-		  updated: new Date(2013, 6, 14), // optional, default = today
-		  generator: "awesome", // optional, default = 'Feed for Node.js'
+		  title: metadata.title,
+		  description: metadata.description,
+		  id: metadata.url,
+		  link: metadata.url,
+		  language: "en",
 		  feedLinks: {
-		    json: "https://example.com/json",
-		    atom: "https://example.com/atom"
+		    json: `${metadata.url}/feed.json`,
+		    rss: `${metadata.url}/feed.xml`,
 		  },
-		  author: {
-		    name: "John Doe",
-		    email: "johndoe@example.com",
-		    link: "https://example.com/johndoe"
-		  }
+		  author: metadata.author
 		});
 
 		posts.forEach(post => {
-			console.log( {post} )
 			feed.addItem({
 			    title: post.title,
 			    id: post.url,
 			    link: post.url,
 			    description: md.render(post.description),
 			    content: md.render(post.description),
-			    date: new Date(
-				    post['createdAt'] ||
-				    post['timestamp'] ||
-				    Date.now()
-			    ),
+			    date: new Date(post['createdAt']),
 			});
 		});
 
